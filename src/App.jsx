5b5260b5af5081f6585047c1da4a3889f5048a4f
SirBeNoble <akeemperkins@gmail.com>
2026-01-31 15:25:36 -0500

    implement HOC for window controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import { Navbar, Welcome, Dock } from "#components";
+import { Draggable } from "gsap/Draggable";
+import { gsap } from "gsap";
+import { Terminal } from "#windows";
+import WindowControls from "#components/WindowControlls";
+
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
@@ -6,6 +12,8 @@ const App = () => {
     <Navbar />
     <Welcome />
     <Dock />
+
+    <Terminal />
   </main>
   )
 };
---
+++ src/windows/index.js
@@ -0,0 +1,4 @@
+import Terminal from "./Terminal";
+import WindowControls from "#components/WindowControlls";
+
+export { Terminal, WindowControls };
